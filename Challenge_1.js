f0beab57a4440cadd6a2b702b1211a7cbadd3cd9
Penachoz <115034797+Penachoz@users.noreply.github.com>
2024-07-31 20:04:16 -0500

    Create Challenge_1.js

---
+++ Challenge_1.js
@@ -0,0 +1,23 @@
+function EvenOrOdd (num){
+    if( num % 2 == 0){
+        console.log("PAR");
+    }
+    else console.log("IMPAR");
+}
+
+const EvenOrOddNew = function(num){
+    if( num % 2 == 0){
+        console.log("PAR");
+    }
+    else console.log("IMPAR");
+}
+const EverOrOddArrow = (num) => {
+    if( num % 2 == 0){
+        console.log("PAR");
+    }
+    else console.log("IMPAR");
+}
+
+EvenOrOdd(1);
+EvenOrOddNew(4);
+EvenOrOddArrow(7);
