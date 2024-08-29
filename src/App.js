de3c0775393b9069822b0d20a9ba10bf9d7a05cd
Penachoz <115034797+Penachoz@users.noreply.github.com>
2024-08-28 19:04:00 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <>
+    <h1> Hola Mundo </h1>
+    <h2> Bienvenido </h2>
+  </>
+  )
+}
+
+export default App;
---
+++ src/FirstAppComponent.js
@@ -0,0 +1,11 @@
+function FirstAppComponent(){
+    return(
+        <>
+            <h1>New title</h1>
+            <span>10</span>
+        </>
+
+    )
+}
+
+export default FirstAppComponent;
